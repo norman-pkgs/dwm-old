bfae2486ddc7d81b74f713eb99cd954bc00acb43
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2021-10-06 19:45:45 +0200

    unified gaps

--- config.h
+++ config.h
@@ -5,10 +5,14 @@ static const unsigned int borderpx       = 0;   /* border pixel of windows */
 static const int corner_radius           = 0;
 static const unsigned int snap           = 8;  /* snap pixel */
 static const int swallowfloating         = 1;   /* 1 means swallow floating windows by default */
-static const unsigned int gappih         = 8;  /* horiz inner gap between windows */
-static const unsigned int gappiv         = 8;  /* vert inner gap between windows */
-static const unsigned int gappoh         = 8;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 8;  /* vert outer gap between windows and screen edge */
+
+static const unsigned int gapall         = 8;
+
+static const unsigned int gappih         = gapall;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = gapall;  /* vert inner gap between windows */
+static const unsigned int gappoh         = gapall;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = gapall;  /* vert outer gap between windows and screen edge */
+
 static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 0;   /* 0 means bottom bar */
