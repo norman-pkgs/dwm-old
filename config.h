c95b508219f686943d9d027655fef3f906839e5d
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-10-24 02:52:52 +0200

    fixed desktop gap spacing

--- config.h
+++ config.h
@@ -7,15 +7,17 @@ static const unsigned int snap           = 8;  /* snap pixel */
 static const int swallowfloating         = 1;   /* 1 means swallow floating windows by default */
 
 #ifdef desktop
-static const unsigned int gapall         = 8;
+static const unsigned int gappih         = 6;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = 6;  /* vert inner gap between windows */
+static const unsigned int gappoh         = 8;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 10;  /* vert outer gap between windows and screen edge */
 #elif laptop
-static const unsigned int gapall         = 16;
+static const unsigned int gappih         = 12;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = 12;  /* vert inner gap between windows */
+static const unsigned int gappoh         = 12;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 12;  /* vert outer gap between windows and screen edge */
 #endif
 
-static const unsigned int gappih         = gapall;  /* horiz inner gap between windows */
-static const unsigned int gappiv         = gapall;  /* vert inner gap between windows */
-static const unsigned int gappoh         = gapall;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = gapall;  /* vert outer gap between windows and screen edge */
 
 static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
@@ -83,8 +85,8 @@ static char urgbordercolor[]             = "#ff0000";
 static char urgfloatcolor[]              = "#db8fd9";
 
 
-static const unsigned int baralpha = 95;
-static const unsigned int borderalpha = 95;
+static const unsigned int baralpha = 100;
+static const unsigned int borderalpha = 100;
 static const unsigned int alphas[][3] = {
     /*                       fg      bg        border     */
     [SchemeNorm]         = { OPAQUE, baralpha, borderalpha },
